c56145a3038e3b60feb01b9e4d58b8e60b49c7cf
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-11-06 23:52:49 +0900

    Fix layers name

--- multiscaleNetmodel.py
+++ multiscaleNetmodel.py
@@ -5,8 +5,6 @@ import numpy as np
 from chainer.initializers import constant, uniform
 
 
-
-
 class MultiscaleNetModel(chainer.Chain):
 
     def __init__(self, n_class):
@@ -18,28 +16,26 @@ class MultiscaleNetModel(chainer.Chain):
         kwargs = {'initialW': uniform.LeCunUniform(scale=1.0)}
 
         # Deep layers
-        self.conv1 = L.Convolution2D(3, 64, 7, stride=2, pad=3, **kwargs),
-        self.conv2_reduce = L.Convolution2D(64, 64, 1, **kwargs),
-        self.conv2 = L.Convolution2D(64, 192, 3, stride=1, pad=1, **kwargs),
-        self.inc3a = L.Inception(192, 64, 96, 128, 16, 32, 32),
-        self.inc3b = L.Inception(256, 128, 128, 192, 32, 96, 64),
-        self.inc4a = L.Inception(480, 192, 96, 208, 16, 48, 64),
-        self.inc4b = L.Inception(512, 160, 112, 224, 24, 64, 64),
-        self.inc4c = L.Inception(512, 128, 128, 256, 24, 64, 64),
-        self.inc4d = L.Inception(512, 112, 144, 288, 32, 64, 64),
-        self.inc4e = L.Inception(528, 256, 160, 320, 32, 128, 128),
-        self.inc5a = L.Inception(832, 256, 160, 320, 32, 128, 128),
-        self.inc5b = L.Inception(832, 384, 192, 384, 48, 128, 128),
-        self.loss3_fc = L.Linear(1024, 1000, **kwargs),
+        self._layers["conv1"] = L.Convolution2D(3, 64, 7, stride=2, pad=3, **kwargs),
+        self._layers["conv2_reduce"] = L.Convolution2D(64, 64, 1, **kwargs),
+        self._layers["conv2"] = L.Convolution2D(64, 192, 3, stride=1, pad=1, **kwargs),
+        self._layers["inc3a"] = L.Inception(192, 64, 96, 128, 16, 32, 32),
+        self._layers["inc3b"] = L.Inception(256, 128, 128, 192, 32, 96, 64),
+        self._layers["inc4a"] = L.Inception(480, 192, 96, 208, 16, 48, 64),
+        self._layers["inc4b"] = L.Inception(512, 128, 128, 256, 24, 64, 64),
+        self._layers["inc4d"] = L.Inception(512, 112, 144, 288, 32, 64, 64),
+        self._layers["inc4e"] = L.Inception(528, 256, 160, 320, 32, 128, 128),
+        self._layers["inc5a"] = L.Inception(832, 256, 160, 320, 32, 128, 128),
+        self._layers["inc5b"] = L.Inception(832, 384, 192, 384, 48, 128, 128),
+        self._layers["loss3_fc"] = L.Linear(1024, 1000, **kwargs),
 
-        self.loss1_conv = L.Convolution2D(512, 128, 1, **kwargs),
-        self.loss1_fc1 = L.Linear(2048, 1024, **kwargs),
-        self.loss1_fc2 = L.Linear(1024, 1000, **kwargs),
+        self._layers["loss1_conv"] = L.Convolution2D(512, 128, 1, **kwargs),
+        self._layers["loss1_fc1"] = L.Linear(2048, 1024, **kwargs),
+        self._layers["loss1_fc2"] = L.Linear(1024, 1000, **kwargs),
 
-        self.loss2_conv = L.Convolution2D(528, 128, 1, **kwargs),
-        self.loss2_fc1 = L.Linear(2048, 1024, **kwargs),
-        self.loss2_fc2 = L.Linear(1024, 1000, **kwargs)
-
+        self._layers["loss2_conv"] = L.Convolution2D(528, 128, 1, **kwargs),
+        self._layers["loss2_fc1"] = L.Linear(2048, 1024, **kwargs),
+        self._layers["loss2_fc2"] = L.Linear(1024, 1000, **kwargs)
 
         # Shallow layers
         self._layers["conv2_1"] = L.Convolution2D(None, 96, 3, stride=4, pad=1, initialW=0.02*np.sqrt(3*3*3))
@@ -61,9 +57,9 @@ class MultiscaleNetModel(chainer.Chain):
 
         # Shallow layers
         h2 = F.average_pooling_2d(x, 4, stride=4, pad=2)
-        h2 = F.max_pooling_2d(F.relu(self.norm_s1(self.conv_s1(h2))), 5, stride=4, pad=1)
+        h2 = F.max_pooling_2d(F.relu(self.norm2_1(self.conv_s1(h2))), 5, stride=4, pad=1)
         h3 = F.average_pooling_2d(x, 8, stride=8, pad=4)
-        h3 = F.max_pooling_2d(F.relu(self.norm_s2(self.conv_s2(h3))), 4, stride=2, pad=1)
+        h3 = F.max_pooling_2d(F.relu(self.norm3_1(self.conv_s2(h3))), 4, stride=2, pad=1)
 
         h23 = F.concat((h2, h3), axis=1)
         h23 = F.normalize(F.reshape(h23, (x.data.shape[0], 3072)))
