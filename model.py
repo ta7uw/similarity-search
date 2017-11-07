afd5c53094785a40c7ae3ea286bb46f898db3e12
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-11-07 14:34:46 +0900

    Revise model class

--- model.py
+++ model.py
@@ -2,73 +2,116 @@ import chainer
 import chainer.links as L
 import chainer.functions as F
 import numpy as np
-from chainer.initializers import constant, uniform
+from chainer.initializers import constant, uniform, normal
 
 
 class MultiscaleNetModel(chainer.Chain):
 
-    def __init__(self, n_class):
-        self._layers = {}
+    def __init__(self, n_class, pretrained_model=None, mean=None, initialW=None, initialBias=None):
         self.n_class = n_class
+        self.mean = mean
+        self.initialbias = initialBias
 
         insize = 224
 
-        # employ default initializers used in BVLC. For more detail, see
-        # https://github.com/chainer/chainer/pull/2424#discussion_r109642209
-        self.initialW = uniform.LeCunUniform(scale=1.0)
+        if mean is None:
+            # imagenet means
+            self.mean = np.array([123.68, 116.779, 103.939], dtype=np.float32)[:, np.newaxis, np.newaxis]
 
-        # Deep layers: GoogleNet of BatchNormalization version
-        self._layers["conv1"] = L.Convolution2D(None, 64, 7, stride=2, pad=3,initialW=self.initialW)
-        self._layers["norm1"] = L.BatchNormalization(64)
-        self._layers["conv2_reduce"] = L.Convolution2D(None, 64, 1, initialW=self.initialW)
-        self._layers["conv2"] = L.Convolution2D(None, 192, 3, stride=1, pad=1, initialW=self.initialW)
-        self._layers["norm2"] = L.BatchNormalization(192)
-        self._layers["inc3a"] = L.Inception(None, 64, 96, 128, 16, 32, 32)
-        self._layers["inc3b"] = L.Inception(None, 128, 128, 192, 32, 96, 64)
-        self._layers["inc4a"] = L.Inception(None, 192, 96, 208, 16, 48, 64)
-        self._layers["inc4b"] = L.Inception(None, 128, 128, 256, 24, 64, 64)
-        self._layers["inc4d"] = L.Inception(None, 112, 144, 288, 32, 64, 64)
-        self._layers["inc4e"] = L.Inception(None, 256, 160, 320, 32, 128, 128)
-        self._layers["inc5a"] = L.Inception(None, 256, 160, 320, 32, 128, 128)
-        self._layers["inc5b"] = L.Inception(None, 384, 192, 384, 48, 128, 128)
-        self._layers["loss3_fc"] = L.Linear(None, self.n_class, initialW=self.initialW)
+        if initialW is None:
+            # employ default initializers used in BVLC. For more detail, see
+            self.initialW = uniform.LeCunUniform(scale=1.0)
 
-        self._layers["loss1_conv"] = L.Convolution2D(None, 128, 1, initialW=self.initialW)
-        self._layers["norma"] = L.BatchNormalization(128)
-        self._layers["loss1_fc1"] = L.Linear(None, 1024, initialW=self.initialW)
-        self._layers["norma2"] = L.BatchNormalization(1024)
-        self._layers["loss1_fc2"] = L.Linear(None, self.n_class, initialW=self.initialW)
+        if pretrained_model:
+            # As a sampling process is time-consuming
+            # we employ a zero initializer for faster computation
+            self.initialW = constant.Zero()
 
-        self._layers["loss2_conv"] = L.Convolution2D(None, 128, 1, initialW=self.initialW)
-        self._layers["normb"] = L.BatchNormalization(128)
-        self._layers["loss2_fc1"] = L.Linear(None, 1024, initialW=self.initialW)
-        self._layers["normb2"] = L.BatchNormalization(1024)
-        self._layers["loss2_fc2"] = L.Linear(None, 1000, initialW=self.initialW)
+        super(MultiscaleNetModel, self).__init__()
+        with self.init_scope():
+            # Deep layers: GoogleNet of BatchNormalization version
+            self.conv1 = L.Convolution2D(None, 64, 7, stride=2, pad=3, initialW=self.initialW)
+            self.norm1 = L.BatchNormalization(64)
+            self.conv2_reduce = L.Convolution2D(None, 64, 1)
+            self.conv2 = L.Convolution2D(None, 192, 3, stride=1, pad=1, initialW=self.initialW)
+            self.norm2 = L.BatchNormalization(192)
+            self.inc3a = L.InceptionBN(None, 64, 64, 64, 64, 96, "avg", 32)
+            self.inc3b = L.InceptionBN(None, 64, 64, 96, 64, 96, "avg", 64)
+            self.inc3c = L.InceptionBN(None, 0, 128, 160, 64, 96, "max", stride=2)
+            self.inc4a = L.InceptionBN(None, 224, 64, 96, 96, 128, "avg", 128)
+            self.inc4b = L.InceptionBN(None, 192, 96, 128, 96, 128, "avg", 128)
+            self.inc4c = L.InceptionBN(None, 128, 128, 160, 128, 160, "avg", 128)
+            self.inc4d = L.InceptionBN(None, 64, 128, 192, 160, 192, "avg", 128)
+            self.inc4e = L.InceptionBN(None, 0, 128, 192, 192, 256, "max", stride=2)
+            self.inc5a = L.InceptionBN(None, 352, 192, 320, 160, 224, "avg", 128)
+            self.inc5b = L.InceptionBN(None, 352, 192, 320, 192, 224, "max", 128)
+            self.loss3_fc = L.Linear(None, self.n_class, initialW=self.initialW)
 
-        # Shallow layers
-        self._layers["conv2_1"] = L.Convolution2D(None, 96, 3, stride=4, pad=1, initialW=0.02*np.sqrt(3*3*3))
-        self._layers["norm2_1"] = L.BatchNormalization(96)
-        self._layers["conv3_1"] = L.Convolution2D(None, 96, 3, stride=4, pad=1, initialW=0.02*np.sqrt(3*3*3))
-        self._layers["norm3_1"] = L.BatchNormalization(96)
+            self.loss1_conv = L.Convolution2D(None, 128, 1, initialW=self.initialW)
+            self.norma = L.BatchNormalization(128)
+            self.loss1_fc1 = L.Linear(None, 1024, initialW=self.initialW)
+            self.norma2 = L.BatchNormalization(1024)
+            self.loss1_fc2 = L.Linear(None, self.n_class, initialW=self.initialW)
 
-        # Final layers
-        self._layers["fc4_1"] = L.Linear(4096, 4096)
-        self._layers["fc4_2"] = L.Linear(4096, self.n_class)
+            self.loss2_conv = L.Convolution2D(None, 128, 1, initialW=self.initialW)
+            self.normb = L.BatchNormalization(128)
+            self.loss2_fc1 = L.Linear(None, 1024, initialW=self.initialW)
+            self.normb2  = L.BatchNormalization(1024)
+            self.loss2_fc2  = L.Linear(None, 1000, initialW=self.initialW)
 
-        super(MultiscaleNetModel, self).__init__(**self._layers)
+            # Shallow layers
+            self.conv_s1 = L.Convolution2D(None, 96, 3, stride=4, pad=1, initialW=0.02*np.sqrt(3*3*3))
+            self.norm_s1 = L.BatchNormalization(96)
+            self.conv_s2 = L.Convolution2D(None, 96, 3, stride=4, pad=1, initialW=0.02*np.sqrt(3*3*3))
+            self.norm_s2 = L.BatchNormalization(96)
+
+            # Final layers
+            self.fc4_1 = L.Linear(None, 4096)
+            self.fc4_2 = L.Linear(None, self.n_class)
 
     def __call__(self, x):
 
         # Deep layers
-        h1 = F.max_pooling_2d(F.relu(self._layers["norm1"](self._layers["conv1"](x))), 3, stride=2, pad=1)
+        h1 = F.max_pooling_2d(F.relu(self.norm1(self.conv1(x))), 3, stride=2, pad=1)
+        h1 = F.relu(self.conv2_reduce(h1))
+        h1 = F.max_pooling_2d(F.relu(self.norm2(self.conv2(h1))), 3, stride=2, pad=1)
 
-        h1 = F.normalize(h1)
+        h1 = self.inc3a(h1)
+        h1 = self.inc3b(h1)
+        h1 = self.inc3c(h1)
+        h1 = self.inc4a(h1)
+
+        a = F.average_pooling_2d(h1, 5, stride=3)
+        a = F.relu(self.norma(self.loss1_conv(a)))
+        a = F.relu(self.norma2(self.loss1_fc1(a)))
+        a = self.loss1_fc2(a)
+        loss1 = F.softmax_cross_entropy(a)
+
+        h1 = self.inc4b(h1)
+        h1 = self.inc4c(h1)
+        h1 = self.inc4d(h1)
+
+        b = F.average_pooling_2d(h1, 5, stride=3)
+        b = F.relu(self.normb(self.loss2_conv(b)))
+        b = F.relu(self.normb2(self.loss2_fc1(b)))
+        b = self.loss2_fc2(b)
+        loss2 = F.softmax_cross_entropy(b)
+
+        h1 = self.inc4e(h1)
+        h1 = self.inc5a(h1)
+        h1 = F.average_pooling_2d(self.inc4e(h1), 7)
+        h1 = self.loss3_fc(h1)
+        loss3 = F.softmax_cross_entropy(h1)
+
+        deep_loss = 0.03 * (loss1 + loss2) + loss3
+
+        h1 = F.normalize(deep_loss)
 
         # Shallow layers
         h2 = F.average_pooling_2d(x, 4, stride=4, pad=2)
-        h2 = F.max_pooling_2d(F.relu(self.norm2_1(self.conv_s1(h2))), 5, stride=4, pad=1)
+        h2 = F.max_pooling_2d(F.relu(self.norm_s1(self.conv_s1(h2))), 5, stride=4, pad=1)
         h3 = F.average_pooling_2d(x, 8, stride=8, pad=4)
-        h3 = F.max_pooling_2d(F.relu(self.norm3_1(self.conv_s2(h3))), 4, stride=2, pad=1)
+        h3 = F.max_pooling_2d(F.relu(self.norm_s2(self.conv_s2(h3))), 4, stride=2, pad=1)
 
         h23 = F.concat((h2, h3), axis=1)
         h23 = F.normalize(F.reshape(h23, (x.data.shape[0], 3072)))
@@ -77,7 +120,17 @@ class MultiscaleNetModel(chainer.Chain):
 
         h = F.normalize(F.relu(self.fc4_1(h)))
         h = self.fc4_2(h)
-        return h
+
+        accuracy = F.accuracy(h)
+
+        loss = F.softmax_cross_entropy(h)
+
+        chainer.report({
+            "deep_loss": deep_loss,
+            "loss" : loss,
+            "accuracy": accuracy,
+        }, self)
+        return loss
 
 
 
