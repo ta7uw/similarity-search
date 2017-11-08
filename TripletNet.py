257f2b418f62ececf810e310e68ca6e26f2e0a52
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-11-08 17:09:46 +0900

    Implement TripletNet

---
+++ TripletNet.py
@@ -0,0 +1,20 @@
+import chainer
+import chainer.functions as F
+from model import MultiscaleNet
+
+
+class TripletNet(chainer.Chain):
+
+    def __init__(self, model):
+        super(TripletNet, self).__init__()
+        with self.init_scope():
+            self.model = model
+
+    def __call__(self, anchor, positive, negative):
+        self.anchor = self.model(anchor)
+        self.positive = self.model(positive)
+        self.negative = self.model(negative)
+        self.loss = F.triplet(anchor=self.anchor, positive=self.positive, negative=self.negative)
+
+        return self.loss
+
