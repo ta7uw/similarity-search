7be8d1afafd331d51ddc6fce3ef9314c14ea1889
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-11-10 14:05:55 +0900

    Add dataset_label.py

--- func/dataset_function.py
+++ func/dataset_function.py
@@ -1,17 +1,23 @@
 import glob
 import chainer
 from itertools import chain
-
+import os
 
 def dataset_label():
     # Image directory name
     IMG_DIR = "dataset"
 
-    # Each item image directory name
+    # Each directory name of item
     b_names = glob.glob("{}/*".format(IMG_DIR))
 
     # List of path to image files
     fnames = [glob.glob("{}/*.jpg".format(b)) for b in b_names]
     fnames = list(chain.from_iterable(fnames))
+
+    # A unique ID is given each directory name
+    labels = [os.path.basename(os.path.dirname(fn)) for fn in fnames]
+    b_names = [os.path.basename(b) for b in b_names]
+    labels = [b_names.index(l) for l in labels]
+    return b_names, labels
 
 
