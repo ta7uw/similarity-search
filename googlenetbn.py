d483242e0fed69d57b08e3369a4b67dea2461587
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-11-11 17:47:08 +0900

    Create tuned cafffe model

--- googlenetbn.py
+++ googlenetbn.py
@@ -10,12 +10,15 @@ class GoogleNetBN(chainer.Chain):
     GoogleNet of BatchNormalization version
     """
 
-    def __init__(self, n_class, pretrained_model=None, mean=None, initialW=None, initialBias=None):
+    def __init__(self, n_class=None, pretrained_model=None, mean=None, initialW=None, initialBias=None):
         self.n_class = n_class
         self.mean = mean
         self.initialbias = initialBias
 
         insize = 224
+
+        if n_class is None:
+            self.n_class = 1000
 
         if mean is None:
             # imagenet means
@@ -37,29 +40,32 @@ class GoogleNetBN(chainer.Chain):
             self.norm1 = L.BatchNormalization(64)
             self.conv2 = L.Convolution2D(None, 192, 3, stride=1, pad=1, nobias=True)
             self.norm2 = L.BatchNormalization(192)
+
             self.inc3a = L.InceptionBN(None, 64, 64, 64, 64, 96, "avg", 32)
             self.inc3b = L.InceptionBN(None, 64, 64, 96, 64, 96, "avg", 64)
             self.inc3c = L.InceptionBN(None, 0, 128, 160, 64, 96, "max", stride=2)
+
             self.inc4a = L.InceptionBN(None, 224, 64, 96, 96, 128, "avg", 128)
             self.inc4b = L.InceptionBN(None, 192, 96, 128, 96, 128, "avg", 128)
             self.inc4c = L.InceptionBN(None, 128, 128, 160, 128, 160, "avg", 128)
             self.inc4d = L.InceptionBN(None, 64, 128, 192, 160, 192, "avg", 128)
             self.inc4e = L.InceptionBN(None, 0, 128, 192, 192, 256, "max", stride=2)
+
             self.inc5a = L.InceptionBN(None, 352, 192, 320, 160, 224, "avg", 128)
             self.inc5b = L.InceptionBN(None, 352, 192, 320, 192, 224, "max", 128)
             self.loss3_fc = L.Linear(None, self.n_class, initialW=self.initialW)
 
-            self.loss1_conv = L.Convolution2D(None, 128, 1, initialW=self.initialW)
+            self.loss1_conv = L.Convolution2D(None, 128, 1, initialW=self.initialW, nobias=True)
             self.norma = L.BatchNormalization(128)
-            self.loss1_fc1 = L.Linear(None, 1024, initialW=self.initialW)
+            self.loss1_fc1 = L.Linear(None, 1024, initialW=self.initialW, nobias=True)
             self.norma2 = L.BatchNormalization(1024)
             self.loss1_fc2 = L.Linear(None, self.n_class, initialW=self.initialW)
 
-            self.loss2_conv = L.Convolution2D(None, 128, 1, initialW=self.initialW)
+            self.loss2_conv = L.Convolution2D(None, 128, 1, initialW=self.initialW, nobias=True)
             self.normb = L.BatchNormalization(128)
-            self.loss2_fc1 = L.Linear(None, 1024, initialW=self.initialW)
+            self.loss2_fc1 = L.Linear(None, 1024, initialW=self.initialW, nobias=True)
             self.normb2 = L.BatchNormalization(1024)
-            self.loss2_fc2 = L.Linear(None, 1000, initialW=self.initialW)
+            self.loss2_fc2 = L.Linear(None, self.n_class, initialW=self.initialW)
 
     def __call__(self, x, t):
         h = F.max_pooling_2d(F.relu(self.norm1(self.conv1(x))), 3, stride=2, pad=1)
