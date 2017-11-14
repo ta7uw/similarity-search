c122191121e12c24dc593406ab345379fe9736ef
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-11-14 13:09:44 +0900

    Add comment

--- triplet_net.py
+++ triplet_net.py
@@ -3,16 +3,28 @@ import chainer.functions as F
 
 
 class TripletNet(chainer.Chain):
-
+    """
+    This is triplet networks
+    """
     def __init__(self, model):
         super(TripletNet, self).__init__()
         with self.init_scope():
             self.model = model
 
     def __call__(self, anchor, positive, negative):
+        """
+        It takes a triplet of variables as inputs
+        :param anchor:
+        :param positive:
+        :param negative:
+        :return:
+            ~chainer.varibales:
+                A variable holding a scalar that is the loss value calculated.
+        """
         self.anchor = self.model(anchor)
         self.positive = self.model(positive)
         self.negative = self.model(negative)
+        # Using margin = 0.2, reduce = "mean" to triplet function
         self.loss = F.triplet(anchor=self.anchor, positive=self.positive, negative=self.negative)
 
         return self.loss
